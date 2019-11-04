c370f319ff0a1fbbcfef792fb6f4b47ff96b90cd
Marcel Raab <marcel.raab@gmail.com>
2019-11-04 17:14:46 +0100

    Initial commit grayscale

---
+++ 0-0_LoadInstallPackages.R
@@ -0,0 +1,18 @@
+# ******************************************************************************
+# ******************************************************************************
+
+# ===================================================
+# Load and download (if necessary) required packages
+# ===================================================
+
+# use (and install if necessary) pacman package 
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+
+# load and install (if necessary) required packages for this course
+p_load(TraMineR, TraMineRextras, tidyverse, haven, Hmisc, gganimate,
+       RColorBrewer, colorspace, knitr, kableExtra, reshape2, summarytools)
+
+
+# ******************************************************************************
+# ******************************************************************************
