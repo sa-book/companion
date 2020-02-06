655126a0e0c108ba4fda503a1e9eeec633c4d88e
Emanuela Struffolino <34509476+struffoe@users.noreply.github.com>
2020-02-06 13:02:38 +0100

    final_chapter3

--- 0-0_LoadInstallPackages.R
+++ 0-0_LoadInstallPackages.R
@@ -11,7 +11,10 @@ library(pacman)
 
 # load and install (if necessary) required packages for this course
 p_load(TraMineR, TraMineRextras, tidyverse, haven, Hmisc, gganimate,
-       RColorBrewer, colorspace, knitr, kableExtra, reshape2, summarytools)
+       RColorBrewer, colorspace, knitr, kableExtra, reshape2, summarytools, 
+       ecodisc, vegan, MCMCpack, corrplot, ade4)
+
+
 
 
 # ******************************************************************************
