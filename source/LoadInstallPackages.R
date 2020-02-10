c60da7c181c384f9e8d9882199a4f1ba2e26e3c9
Marcel Raab <marcel.raab@gmail.com>
2020-02-10 13:57:17 +0100

    re-built - new yaml

--- source/LoadInstallPackages.R
+++ source/LoadInstallPackages.R
@@ -10,9 +10,12 @@ if (!require("pacman")) install.packages("pacman")
 library(pacman)
 
 # load and install (if necessary) required packages for this course
-p_load(TraMineR, TraMineRextras, tidyverse, haven, Hmisc, gganimate, rmarkdown,
-       sjPlot, RColorBrewer, colorspace, knitr, kableExtra, reshape2, summarytools)
+p_load(TraMineR, TraMineRextras, WeightedCluster, cluster, survival,
+       haven, Hmisc, gganimate, rmarkdown,
+       sjPlot, RColorBrewer, colorspace, knitr, kableExtra, reshape2, 
+       SDMTools, summarytools, MCMCpack, corrplot, ade4, tidyverse)
 
 
 # ******************************************************************************
 # ******************************************************************************
+
